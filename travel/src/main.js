4a0e42c42b1ee81bf9c8b0358fe0913c12811ed0
Yuuulong <yuuuulong@gmail.com>
2019-11-21 20:53:13 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/styles/reset.css'
+// 移动端1px边框问题
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
+import router from './router.js'
+
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
