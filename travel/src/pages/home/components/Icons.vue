71e542b20c346486d4aff923ad605ae3c84cb177
yuuulong <yuuulong@foxmail.com>
2019-11-22 20:57:44 +0800

    index-icons

---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="icons">
+    <swiper >
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" 
+             v-for="item of page" 
+             :key="item.id">
+          <img class="icon-img" :src="item.imgUrl"/>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeIcons",
+  data () {
+    return {
+      iconList: [
+        {
+          id: "001",
+          imgUrl: require("../imgs/piao.png"),
+          desc: '景点门票'
+        },
+        {
+          id: "002",
+          imgUrl: require("../imgs/flight.png"),
+          desc: '滑雪季'
+        },
+        {
+          id: "003",
+          imgUrl: require("../imgs/hotel.png"),
+          desc: '泡温泉'
+        },
+        {
+          id: "004",
+          imgUrl: require("../imgs/package.png"),
+          desc: '动植物园'
+        },
+        {
+          id: "005",
+          imgUrl: require("../imgs/piao.png"),
+          desc: '景点门票'
+        },
+        {
+          id: "006",
+          imgUrl: require("../imgs/hotel.png"),
+          desc: '农家院'
+        },
+        {
+          id: "007",
+          imgUrl: require("../imgs/train.png"),
+          desc: '景点门票'
+        },
+        {
+          id: "008",
+          imgUrl: require("../imgs/flight.png"),
+          desc: '冰雪世界'
+        },{
+          id: "009",
+          imgUrl: require("../imgs/package.png"),
+          desc: '中餐厅'
+        },{
+          id: "010",
+          imgUrl: require("../imgs/train.png"),
+          desc: '一日游'
+        },
+      ]
+    };
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item,index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl';
+
+.icons {
+  // overflow hidden
+  // height 0
+  // padding-bottom 50%
+  .icon {
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-top: 3%;
+    padding-bottom: 20%;
+
+    .icon-img {
+      display: block;
+      width: 50%;
+      margin: 0 auto;
+    }
+  }
+
+  .icon-desc {
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    text-align: center;
+    color: $darkTextColor;
+    overflow hidden;
+    white-space nowrap;
+    text-overflow ellipsis
+  }
+}
+</style>
--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -3,7 +3,7 @@
     <swiper :options="swiperOption">
       <!-- slides -->
       <swiper-slide v-for="item of swiperList" :key="item.id">
-        <img class="swiper-img" :src="item.imgUrl" />
+        <img class="swiper-img" :src="item.imgUrl"/>
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -22,17 +22,17 @@ export default {
         {
           id: "001",
           imgUrl:
-            "https://imgs.qunarzz.com/vc/aa/9e/82/14bf523635542806a671763e98.jpg_92.jpg"
+            require('../imgs/lunbo1.jpg')
         },
         {
           id: "002",
           imgUrl:
-            "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/abe3418971067bd655fb2794d6e2eeec.jpg"
+          require('../imgs/lunbo2.jpg')
         },
         {
           id: "003",
           imgUrl:
-            "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/76e68eda4ed089c0e5b0ce2367efe428.jpg"
+          require('../imgs/lunbo3.jpg')
         }
       ]
     };
