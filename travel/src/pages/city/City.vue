84f85b8728a6f31717cc1bc19d05046a10de68b9
yuuulong <yuuulong@foxmail.com>
2019-11-26 10:41:52 +0800

    city-list-alphabet

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -2,18 +2,25 @@
 <div>
   <city-header></city-header>
   <city-search></city-search>
+  <city-list></city-list>
+  <city-alphabet></city-alphabet>
 </div>
 </template>
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 
-// import axios from 'axios'
+
+
 export default {
   name: "City",
   components: {
     CityHeader,
     CitySearch,
+    CityList,
+    CityAlphabet,
   }
  
 };
---
+++ travel/src/pages/city/components/List.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wraper">
+          <div class="button">沈阳</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">沈阳</div>
+        </div><div class="button-wraper">
+          <div class="button">沈阳</div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wraper">
+          <div class="button">沈阳</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">沈阳</div>
+        </div><div class="button-wraper">
+          <div class="button">沈阳</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+
+      </div>
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+
+      </div><div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+
+      </div><div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+
+      </div><div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+// 引入城市选择字母插件
+import Bscroll from 'better-scroll'
+export default {
+  name: "CityList",
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl';
+
+.border-topbottom, .border-bottom {
+  &:before {
+    border-color: #ccc;
+  }
+
+  &:after {
+    border-color: #ccc;
+  }
+}
+.list
+  position absolute
+  overflow hidden
+  top 1.58rem
+  bottom 0
+  left 0
+  right 0
+.title {
+  line-height: 0.4rem;
+  background: #eee;
+  color: #666;
+  font-size: 0.26rem;
+}
+
+.button-list {
+  padding: 0.1rem 0.6rem 0.1rem 0.1rem;
+  overflow: hidden;
+
+  .button-wraper {
+    float: left;
+    width: 33.33%;
+
+    .button {
+      margin: 0.1rem 0.1rem;
+      padding: 0.1rem 0;
+      text-align: center;
+      border: 0.02rem solid #ccc;
+      border-radius: 0.06rem;
+    }
+  }
+}
+.item-list
+  .item
+    line-height .54rem
+    padding-left .2rem
+
+</style>
