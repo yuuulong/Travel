33589b45be33f0b8924a2c1549ba7b5046db17b5
yuuulong <yuuulong@foxmail.com>
2019-11-26 09:39:43 +0800

    city-search

---
+++ travel/src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或者拼音">
+  </div>
+
+</template>
+<script>
+// import axios from "axios";
+
+export default {
+  name: "CitySearch"
+};
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl';
+  .search
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .search-input
+      box-sizing border-box
+      width 100%
+      height .62rem
+      padding 0 .1rem
+      text-align center
+      border-radius .06rem
+      color #666
+
+
+</style>
